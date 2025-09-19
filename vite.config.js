9fe97bfade79f4328525ec7ebebaf76f3e06fca7
argurjar <gurjarashish21@gmail.com>
2025-09-19 17:18:01 +0530

    build issue resolve

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // default 500 KB → ab 2 MB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          axios: ['axios'],
+          framer: ['framer-motion'],
+          'react-icons': ['react-icons'],
+          'react-router-dom': ['react-router-dom'],
+          'react-toastify': ['react-toastify'],
+          swiper: ['swiper'],
+          // aur installed packages jo chahoge add kar sakte ho
+        },
+      },
+    },
+  },
 })
